91e82fd02f7ba9edac69ae23a84bc849b5d25963
Ilya <semjef@yandex.ru>
2024-12-24 01:39:27 +0300

    try fix docker

--- ha-salute-bridge/rootfs/run.sh
+++ ha-salute-bridge/rootfs/run.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/with-contenv bashio
 
 echo "token"
+echo ${SUPERVISOR_TOKEN}
+echo ${__BASHIO_SUPERVISOR_API}
 export SUPERVISOR_TOKEN="${__BASHIO_SUPERVISOR_API}"
+
 python3 /app/main.py
